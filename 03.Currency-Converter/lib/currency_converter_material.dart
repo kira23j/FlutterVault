93c9162e92ec781bbf7bf9bf7ac5cc79ed554c7f
kira23j <kirubelmamuyesahle@gmail.com>
2024-02-26 13:51:04 -0800

    adding the final ui design for currency converter app

--- 03.Currency-Converter/lib/currency_converter_material.dart
+++ 03.Currency-Converter/lib/currency_converter_material.dart
@@ -5,30 +5,77 @@ class CurrencyConverterMaterialHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-        backgroundColor: Colors.indigo,
+    return Scaffold(
+        backgroundColor: const Color.fromARGB(255, 2, 53, 130),
+        appBar: AppBar(
+          backgroundColor: const Color.fromARGB(255, 2, 53, 130),
+          elevation: 0,
+          title: const Text(
+            "Currency Converter",
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
         body: Center(
           child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-            Text(
-              '0',
-              style: TextStyle(
-                  fontSize: 55,
-                  fontWeight: FontWeight.bold,
-                  color: Color.fromARGB(255, 255, 255, 255)),
-            ),
-            TextField(
-              style: TextStyle(
-                color: Colors.white,
-              ),
-              decoration: InputDecoration(
-                hintText: "please enter the amount in USD",
-                hintStyle: TextStyle(color: Colors.white60),
-                prefixIcon: Icon(Icons.monetization_on),
-                prefixIconColor: Colors.white60,
-                filled: true,
-                fillColor: Colors.black,
+            Container(
+              padding: const EdgeInsets.all(10.0),
+              margin: const EdgeInsets.all(10.0),
+              child: const Text(
+                '0',
+                style: TextStyle(
+                    fontSize: 55,
+                    fontWeight: FontWeight.bold,
+                    color: Color.fromARGB(255, 255, 255, 255)),
               ),
             ),
+            const Padding(
+              padding: EdgeInsets.all(10.0),
+              child: TextField(
+                style: TextStyle(
+                  color: Colors.black87,
+                ),
+                decoration: InputDecoration(
+                  hintText: "please enter the amount in USD",
+                  hintStyle: TextStyle(color: Colors.black54),
+                  prefixIcon: Icon(Icons.monetization_on),
+                  prefixIconColor: Colors.black54,
+                  filled: true,
+                  fillColor: Colors.white,
+                  focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                          color: Colors.black,
+                          width: 2.0,
+                          style: BorderStyle.solid,
+                          strokeAlign: BorderSide.strokeAlignCenter),
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(10),
+                      )),
+                ),
+                keyboardType: TextInputType.numberWithOptions(
+                  decimal: true,
+                  signed: true,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: ElevatedButton(
+                  onPressed: () {
+                    print("button click");
+                  },
+                  style: const ButtonStyle(
+                      backgroundColor: MaterialStatePropertyAll(
+                          Color.fromARGB(255, 2, 1, 27)),
+                      foregroundColor: MaterialStatePropertyAll(Colors.white),
+                      minimumSize:
+                          MaterialStatePropertyAll(Size(double.infinity, 50)),
+                      shape:
+                          MaterialStatePropertyAll(RoundedRectangleBorder())),
+                  child: const Text("Convert")),
+            )
           ]),
         ));
   }
