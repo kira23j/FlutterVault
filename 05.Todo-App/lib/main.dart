4b194fcb809b67da22b9b04d3807ecb4a5688b12
kira23j <kirubelmamuyesahle@gmail.com>
2024-03-10 15:22:14 -0700

    adding state notifier provider

--- 05.Todo-App/lib/main.dart
+++ 05.Todo-App/lib/main.dart
@@ -17,23 +17,68 @@ class MyApp extends StatelessWidget {
 }
 
 // This widget is the root of your application.
-class Home extends StatelessWidget {
+class Home extends ConsumerWidget {
   const Home({super.key});
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    int counter = ref.watch(counterProvider);
+    CounterNotifier counterController = ref.watch(counterProvider.notifier);
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Riverpod Simplified"),
+        title: const Text("Riverpod "),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Consumer(
-              builder: (context, ref, child) {
-                return Text(ref.read(normalProvider));
-              },
+            TextButton(
+                onPressed: () {
+                  Navigator.of(context)
+                      .push(MaterialPageRoute(builder: (context) => Second()));
+                },
+                child: Text("go to secound screen")),
+            Text(
+              "Counter:$counter",
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
             ),
+            TextButton(
+              onPressed: () {
+                counterController.add();
+              },
+              child: const Text("Add"),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Second extends ConsumerWidget {
+  const Second({super.key});
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    int counter = ref.watch(counterProvider);
+    CounterNotifier counterController = ref.watch(counterProvider.notifier);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Riverpod secound"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Counter on secound screen:$counter",
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context)
+                    .push(MaterialPageRoute(builder: (context) => Second()));
+              },
+              child: const Text("Go to second screen"),
+            )
           ],
         ),
       ),
